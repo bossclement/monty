b6c447d31719789b3ef2b87adb5faca01f177e84
bossclement <bossclement@gmail.com>
2023-05-26 01:53:47 +0200

    New commit

--- stack_ops.c
+++ stack_ops.c
@@ -7,7 +7,6 @@
  */
 void pint(stack_t **stack, unsigned int line_number)
 {
-	stack_t *head = *stack;
 	char msg[1024];
 
 	if (*stack == NULL)
@@ -15,13 +14,7 @@ void pint(stack_t **stack, unsigned int line_number)
 		sprintf(msg, "L%u: can't pint, stack empty", line_number);
 		error(msg, *stack, EXIT_FAILURE);
 	}
-	while (head->next)
-		head = head->next;
-	while (head)
-	{
-		printf("%d\n", head->n);
-		head = head->prev;
-	}
+	printf("%d\n", (*stack)->n);
 }
 
 /**
