66bbdb9d76dbf26893ebf9ad8d1b881fad4dfc80
Otuekong <myshield02@gmail.com>
2023-05-25 17:51:44 +0100

    code for stack ops

---
+++ stack_ops.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * pint - prints the value at the top of the stack
+ * @stack: pointer to a node
+ * @line_number: int var - line number of node
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+}
+
