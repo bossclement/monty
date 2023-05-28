8ffe55e35ddd6c3dea7771e0662332fcdf630b4e
bossclement <bossclement@gmail.com>
2023-05-28 23:55:52 +0200

    New commit

--- funcs_1.c
+++ funcs_1.c
@@ -25,7 +25,7 @@ void mul(stack_t **stack, unsigned int line_number)
 }
 
 /**
- * mod -   computes the rest of the division of the second top element
+ * mod - computes the rest of the division of the second top element
  *	of the stack by the top element of the stack.
  * @stack: pointer to a node.
  * @line_number: int var - line number of node.
@@ -50,4 +50,27 @@ void mod(stack_t **stack, unsigned int line_number)
 	next->prev = NULL;
 	*stack = next;
 	free(head);
+}
+
+/**
+ * pchar - prints the char at the top of the stack.
+ * @stack: pointer to a node.
+ * @line_number: int var - line number of node.
+ */
+void pchar(stack_t **stack, unsigned int line_number __attribute__((unused)))
+{
+	char msg[1024];
+
+	if (*stack == NULL)
+	{
+		sprintf(msg, "L%u: can't pchar, stack empty", line_number + 1);
+		error(msg, *stack, EXIT_FAILURE);
+	}
+	else if ((*stack)->n > 127)
+	{
+		sprintf(msg, "L%u: can't pchar, value out of range", line_number + 1);
+		error(msg, *stack, EXIT_FAILURE);
+	}
+
+	printf("%c\n", (*stack)->n);
 }
